f8348679c92a588e3d2f532ef85321a945a37f8b
Douglas Bebber <dab@i2pmail.org>
2023-05-20 09:10:39 -0500

    Update authbot.py
    minor edit.

--- authbot.py
+++ authbot.py
@@ -2,6 +2,7 @@
 # AuthBot version 0.0.9
 # Copyright (C) - 2023
 # by Douglas Alan Bebber
+# @dougbebber:matrix.org
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”),
 # to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
